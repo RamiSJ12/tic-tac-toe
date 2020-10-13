5cf87c9625261af9f0fb4a4fee25e81e623ce8a4
martinnajjar12 <martin_najjar@yahoo.com>
2020-10-13 21:26:57 +0300

    Set up new linters

--- spec/table_spec.rb
+++ spec/table_spec.rb
@@ -1,5 +1,6 @@
 require './lib/table.rb'
 require './lib/colors.rb'
+# rubocop:disable Metrics/BlockLength
 
 describe Table do
   describe '#initialize' do
@@ -18,15 +19,15 @@ describe Table do
       it 'returns true if the index is greater than 9' do
         expect(new_table.invalid_move(10)).to eql true
       end
-  
+
       it 'returns true if the index is less than 1' do
         expect(new_table.invalid_move(0)).to eql true
       end
-  
+
       it 'returns false when the index is between 1..9' do
         expect(new_table.invalid_move(2)).to eql false
       end
-  
+
       it 'returns true when the index is x or o' do
         new_table.modify_table(9, 'x')
         expect(new_table.invalid_move(9)).to eql true
@@ -37,56 +38,56 @@ describe Table do
       it 'returns nil' do
         expect(new_table.check_win).to eql nil
       end
-  
+
       it 'returns 1 if winner wins with a row' do
         new_table.modify_table(1, 'x')
         new_table.modify_table(2, 'x')
         new_table.modify_table(3, 'x')
         expect(new_table.check_win).to eql 1
       end
-  
+
       it 'returns 1 if winner wins with a row' do
         new_table.modify_table(4, 'x')
         new_table.modify_table(5, 'x')
         new_table.modify_table(6, 'x')
         expect(new_table.check_win).to eql 1
       end
-  
+
       it 'returns 1 if winner wins with a row' do
         new_table.modify_table(7, 'x')
         new_table.modify_table(8, 'x')
         new_table.modify_table(9, 'x')
         expect(new_table.check_win).to eql 1
       end
-  
+
       it 'returns 2 if winner wins with a column' do
         new_table.modify_table(1, 'x')
         new_table.modify_table(4, 'x')
         new_table.modify_table(7, 'x')
         expect(new_table.check_win).to eql 2
       end
-  
+
       it 'returns 2 if winner wins with a column' do
         new_table.modify_table(2, 'x')
         new_table.modify_table(5, 'x')
         new_table.modify_table(8, 'x')
         expect(new_table.check_win).to eql 2
       end
-  
+
       it 'returns 2 if winner wins with a column' do
         new_table.modify_table(3, 'x')
         new_table.modify_table(6, 'x')
         new_table.modify_table(9, 'x')
         expect(new_table.check_win).to eql 2
       end
-  
+
       it 'returns 3 if winner wins with a diagonal' do
         new_table.modify_table(3, 'x')
         new_table.modify_table(5, 'x')
         new_table.modify_table(7, 'x')
         expect(new_table.check_win).to eql 3
       end
-  
+
       it 'returns 3 if winner wins with a diagonal' do
         new_table.modify_table(1, 'x')
         new_table.modify_table(5, 'x')
@@ -105,3 +106,5 @@ describe Table do
     end
   end
 end
+
+# rubocop:enable Metrics/BlockLength
