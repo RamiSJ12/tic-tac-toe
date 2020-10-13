2bbd4afd0120e6cf44215705b3faa4276c8283bd
martinnajjar12 <martin_najjar@yahoo.com>
2020-10-14 01:04:20 +0300

    Add paranthesis after .to eql

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -4,11 +4,11 @@ describe Player do
     let(:martin) { Player.new('Martin', 'x') }
     let(:lalo) { Player.new('Lalo') }
     it 'returns Martin' do
-      expect(martin.name).to eql 'Martin'
+      expect(martin.name).to eql('Martin')
     end
 
     it 'returns x' do
-      expect(martin.sign).to eql 'x'
+      expect(martin.sign).to eql('x')
     end
   end
 end
